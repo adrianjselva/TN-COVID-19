5658276336519c34a0bb28a3ab91cf7a19064ddb
Adrian Selva <adrianjselva@gmail.com>
2020-08-11 14:24:21 -0500

    Changed county school URL

--- tdoh_datasets.R
+++ tdoh_datasets.R
@@ -66,11 +66,11 @@ curr_dataset_date <- function(lof_df) {
 
 dataset_dir <- function() {
   dirs <- list.files("datasets")
-  
+
   if(length(dirs) == 0) {
     return(NULL)
   }
-  
+
   dirs <- lapply(dirs, function(x) {as.Date(x, format = "%m-%d-%Y")})
   dirs <- do.call(c, dirs)
   return(dirs)
@@ -78,11 +78,11 @@ dataset_dir <- function() {
 
 counties_dir <- function() {
   dirs <- list.files("counties")
-  
+
   if(length(dirs) == 0) {
     return(NULL)
   }
-  
+
   dirs <- lapply(dirs, function(x) {as.Date(x, format = "%m-%d-%Y")})
   dirs <- do.call(c, dirs)
   return(dirs)
@@ -105,27 +105,27 @@ del_temp <- function() {
 }
 
 split_by_date <- function(dataset) {
-  date_list <- list() 
-  
+  date_list <- list()
+
   for(date_r in unique(dataset[[1]])) {
     d_data <- list(dataset[dataset[[1]] == date_r,])
-    
+
     date_list <- append(date_list, d_data)
   }
-  
+
   return(date_list)
 }
 
 split_by_county <- function(dataset) {
   county_list <- list()
   cnames <- c()
-  
+
   for(c_name in unique(dataset$COUNTY)) {
-    
+
     if(c_name == "Out of State") {
       next
     }
-    
+
     c_data <- dataset[dataset$COUNTY == c_name,]
     f_code <- fips[fips$county == c_name,1]
     if(length(f_code) == 0) {
@@ -134,24 +134,24 @@ split_by_county <- function(dataset) {
     c_data$FIPS <- f_code
     c_data$COUNTY <- tolower(c_data$COUNTY)
     c_data$COUNTY <- tools::toTitleCase(c_data$COUNTY)
-    
+
     if(c_data$COUNTY[1] == "Mcminn") {
       c_data$COUNTY <- rep("McMinn", length(c_data$COUNTY))
     }
-    
+
     if(c_data$COUNTY[1] == "Mcnairy") {
       c_data$COUNTY <- rep("McNairy", length(c_data$COUNTY))
     }
-    
+
     cnames <- c(cnames, c_data$COUNTY[1])
-    
+
     c_data <- list(c_data)
-    
+
     county_list <- append(county_list, c_data)
   }
-  
+
   names(county_list) <- cnames
-  
+
   return(county_list)
 }
 
@@ -161,17 +161,17 @@ county_name_path <- function(path, county_name) {
 }
 
 first_nonzero_index <- function(col) {
-  
+
   if(unique(col)[1] == 0 && length(unique(col)) == 1) {
     return(1)
   }
-  
+
   i <- 1
   i_found <- FALSE
-  
+
   while(i_found == FALSE) {
     i <- i + 1
-    
+
     if(col[i] != 0) {
       return(i)
     }
@@ -180,7 +180,7 @@ first_nonzero_index <- function(col) {
 
 seven_day_average <- function(daily_cases) {
   mov_avg <- daily_cases
-  
+
   for(i in 1:length(daily_cases)) {
     if(i < 7) {
       mov_avg[i] <- (sum(daily_cases[1:i]) / i)
@@ -188,7 +188,7 @@ seven_day_average <- function(daily_cases) {
       mov_avg[i] <- (sum(daily_cases[(i-6):i]) / 7)
     }
   }
-  
+
   return(mov_avg)
 }
 
@@ -196,19 +196,19 @@ add_new_positive_tests_cty <- function(super) {
   if("NEW_POS_TESTS" %in% names(super[[1]])) {
     return(super)
   }
-  
+
   npt <- lapply(super, function(cty) {
     cty$NEW_POS_TESTS <- c(0, diff(cty$POS_TESTS))
     cty$NEW_POS_TESTS[(first_nonzero_index(cty$NEW_POS_TESTS))] <- 0
-    
+
     pos_test_index <- which(colnames(cty) == "POS_TESTS")
-    
+
     cty <- cty[, c(1:(pos_test_index), ncol(cty), (pos_test_index + 2):(ncol(cty) - 1))]
-    
+
     return(cty)
   })
 
-  
+
   return(npt)
 }
 
@@ -216,34 +216,34 @@ add_new_positive_tests_state <- function(ds) {
   if("NEW_POS_TESTS" %in% colnames(ds)) {
     return(ds)
   }
-  
+
   ds$NEW_POS_TESTS <- c(0, diff(ds$POS_TESTS))
   ds$NEW_POS_TESTS[(first_nonzero_index(ds$NEW_POS_TESTS))] <- 0
-  
+
   pos_test_index <- which(colnames(ds) == "POS_TESTS")
-    
+
   ds <- ds[, c(1:(pos_test_index), ncol(ds), (pos_test_index + 2):(ncol(ds) - 1))]
-  
+
   return(ds)
 }
 
 rename_specimen_col <- function(sl) {
   nsl <- lapply(sl, function(cty) {
     colnames(cty)[colnames(cty) == "COUNTY_CASE_COUNT"] <- "NEW_CASES"
-    
-    cty <- cty %>% 
+
+    cty <- cty %>%
       complete(DATE = seq.Date(min(DATE), max(DATE), by="day"), fill = list(NEW_CASES = 0, COUNTY = cty$COUNTY[1]))
-    
+
     return(cty)
   })
-  
+
   return(nsl)
 }
 
 create_state_specimen_collection <- function(ds) {
   nscds <- aggregate(ds["COUNTY_CASE_COUNT"], by=ds["DATE"], sum)
   colnames(nscds) <- c('DATE', "NEW_CASES")
-  
+
   return(nscds)
 }
 
@@ -344,7 +344,7 @@ age_county_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/ce
 county_new_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-County-New.XLSX"
 daily_case_info_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Daily-Case-Info.XLSX"
 race_ethnicity_sex_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-RaceEthSex.XLSX"
-county_school_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Daily-County-School.XLSX"
+county_school_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Daily-County-Cases-5-18-Years.XLSX"
 specimen_collection_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-MMWR-Week-Case-Count.XLSX"
 age_outcomes_dataset_url <- "https://www.tn.gov/content/dam/tn/health/documents/cedep/novel-coronavirus/datasets/Public-Dataset-Daily-Age-Group-Outcomes.XLSX"
 
@@ -378,7 +378,7 @@ download.file(url = age_outcomes_dataset_url, destfile = age_outcomes_temp_path)
 
 print("Loading files...")
 
-age_col_types <- c("date", "text", "numeric", "numeric", "numeric", 
+age_col_types <- c("date", "text", "numeric", "numeric", "numeric",
                    "numeric", "numeric", "numeric")
 
 age_county_col_types <- c("date", "text", "text", "numeric")
@@ -438,12 +438,12 @@ if(is.element(curr_date, dataset_dir())) {
   print(paste("Dataset files do not exist. Creating dataset files for ", fcurr_date, " ...", sep = ""))
   dpath <- dataset_path(curr_date)
   dir.create(dpath)
-  
+
   tfiles <- list.files("temp")
-  
+
   ffiles <- lapply(tfiles, function(x) {paste("temp", x, sep="/")})
   ffiles <- do.call(c, ffiles)
-  
+
   file.copy(from = ffiles, to = dpath, overwrite = TRUE, recursive = FALSE, copy.mode = TRUE)
   print("Deleting temporary files...")
   del_temp()
@@ -465,8 +465,8 @@ if(is.element(curr_date, counties_dir())) {
   msg <- paste("Creating county .csv files for ", fcurr_date, "...", sep = "")
   print(msg)
   cpath <- counties_path(curr_date)
-  dir.create(cpath) 
-  
+  dir.create(cpath)
+
   for (county in county_new_superlist) {
     county %>% write.csv(county_name_path(cpath, county$COUNTY), row.names = FALSE)
   }
@@ -475,46 +475,46 @@ if(is.element(curr_date, counties_dir())) {
 
 msg <- paste("Creating plotly JSON files for ", fcurr_date, "...", sep = "")
 print(msg)
-  
+
 c_list <- list()
 c_name_list <- c()
-  
+
 for (county in county_new_superlist) {
   c_name <- county$COUNTY[1]
   c_name_list <- c(c_name_list, c_name)
-    
-  l_names <- c("total_cases", "total_deaths", "daily_cases", "daily_deaths", 
-               "testing", "active_cases", "daily_active", "total_recoveries", 
+
+  l_names <- c("total_cases", "total_deaths", "daily_cases", "daily_deaths",
+               "testing", "active_cases", "daily_active", "total_recoveries",
                "daily_recoveries", "total_hospitalized", "daily_hospitalized",
                "daily_cases_specimen")
-  
+
   tcp <- total_cases_plot(county)
   tdp <- total_deaths_plot(county)
-    
+
   dcp <- daily_cases_plot(county)
   ddp <- daily_deaths_plot(county)
-  
+
   dtest <- daily_testing_data_plot(county)
-  
+
   cac <- current_active_cases_plot(county)
   dac <- daily_active_cases_plot(county)
-  
+
   tr <- total_recovered_plot(county)
   dr <- daily_recovered_plot(county)
-  
+
   th <- total_hospitalized_plot(county)
   dh <- daily_hospitalizations_plot(county)
-  
+
   dcsp <- daily_cases_specimen_plot(specimen_collection_superlist[[c_name]])
-  
+
   plot_list <- list(tcp, tdp, dcp, ddp, dtest, cac, dac, tr, dr, th, dh, dcsp)
   names(plot_list) <- l_names
-    
+
   c_list <- append(c_list, list(plot_list))
 }
-  
+
 names(c_list) <- c_name_list
-  
+
 json_list <- toJSON(c_list)
 write(json_list, file = "output/county_plots.json")
 
@@ -530,13 +530,13 @@ drm <- daily_recovered_map(county_new_superlist)
 thm <- total_hospitalizations_map(county_new_superlist)
 dhm <- daily_hospitalizations_map(county_new_superlist)
 dcsm <- daily_cases_map(specimen_collection_superlist)
-   
+
 cmap_list <- list(tcm, tdm, dcm, ddm, tm, acm, dacm, trm, drm, thm, dhm, dcsm)
 names(cmap_list) <- l_names
-   
+
 json_cmap_list <- toJSON(cmap_list)
 write(json_cmap_list, file = 'output/county_maps.json')
-  
+
 tcsm <- total_cases_state_map()
 tdsm <- total_deaths_state_map()
 dcsm <- daily_cases_state_map()
@@ -549,7 +549,7 @@ drsm <- daily_recovered_state_map()
 thsm <- total_hospitalizations_state_map()
 dhsm <- daily_hospitalizations_state_map()
 dcssm <- daily_cases_specimen_state_map()
-  
+
 smap_list <- list(tcsm, tdsm, dcsm, ddsm, tsm, acsm, dacsm, trsm, drsm, thsm, dhsm, dcssm)
 names(smap_list) <- l_names
 
@@ -568,7 +568,7 @@ drsp <- daily_recovered_state_plot()
 thsp <- total_hospitalizations_state_plot()
 dhsp <- daily_hospitalizations_state_plot()
 dcssp <- daily_cases_specimen_state_plot(specimen_collection_dataset)
-  
+
 state_plot_list <- list(tcsp, tdsp, dcsp, ddsp, tsp, acsp, dacsp, trsp, drsp, thsp, dhsp, dcssp)
 names(state_plot_list) <- l_names
 
